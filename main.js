ded0b1db3424b070b7ce52d233c28a6b2e169935
Patty Nunez <nunezmacias@gmail.com>
2020-01-02 18:09:50 -0800

    Making CSS & HTML adjustments

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
-    document.getElementById("navpanel").style.width = "38%";
-    document.getElementById("navpanel").style.backgroundColor = "rgb(28,51,104)"
+    document.getElementById("navpanel").style.width = "37%";
+    document.getElementById("navpanel").style.backgroundColor = "rgba(28,51,104, .5)"
   }
   
   /* Set the width of the sidebar to 0 (hide it) */
